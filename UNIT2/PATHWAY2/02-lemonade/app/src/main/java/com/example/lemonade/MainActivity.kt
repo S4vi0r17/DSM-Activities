1e35e415b13839003068cb8dc03439755554e48f
S4v!0r <gbp17@outlook.com>
2025-09-26 14:27:37 -0500

    feat: Enhance Lemonade app with interactive steps and dynamic squeeze count display

--- UNIT2/PATHWAY2/02-lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ UNIT2/PATHWAY2/02-lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,16 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -48,19 +51,99 @@ fun LemonadeApp() {
 
 @Composable
 fun ImagenWithDescriptionOfLemonade(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = { }) {
-            Image(
-                painter = painterResource(R.drawable.lemon_tree),
-                contentDescription = "Step 1"
+
+    var currentStep by remember { mutableIntStateOf(2) }
+    var squeezeCount by remember { mutableIntStateOf(0) }
+
+    when (currentStep) {
+        1 -> {
+            LemonTextAndImage(
+                R.drawable.lemon_tree,
+                R.string.lemon_tree_content_description,
+                {
+                    currentStep = 2
+                    squeezeCount = (2..4).random()
+                },
+                R.string.lemon_tree_instructions
             )
         }
-        Spacer(modifier = Modifier.height(16.dp))
+
+        2 -> {
+
+
+            LemonTextAndImage(
+                R.drawable.lemon_squeeze,
+                R.string.lemon_content_description,
+                {
+                    squeezeCount--
+                    if (squeezeCount <= 0) currentStep = 3
+                },
+                R.string.squeeze_lemon_instructions,
+                Modifier,
+                squeezeCount
+            )
+        }
+
+        3 -> {
+            LemonTextAndImage(
+                R.drawable.lemon_drink,
+                R.string.full_glass_content_description,
+                { currentStep = 4 },
+                R.string.drink_lemonade_instructions
+            )
+
+        }
+
+        4 -> {
+            LemonTextAndImage(
+                R.drawable.lemon_restart,
+                R.string.empty_glass_content_description,
+                { currentStep = 1 },
+                R.string.restart_instructions
+            )
+        }
+    }
+}
+
+@Composable
+fun LemonTextAndImage(
+    painter: Int,
+    contentDescription: Int,
+    onImageClick: () -> Unit,
+    instruction: Int,
+    modifier: Modifier = Modifier,
+    count: Int? = null,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { onImageClick() }) {
+            Image(
+                painter = painterResource(painter),
+                contentDescription = stringResource(contentDescription),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        if (count != null) {
+            Text(
+                text = count.toString(),
+                fontSize = 28.sp
+            )
+        } else {
+            Text(
+                text = " ",
+                fontSize = 28.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
         Text(
-            text = stringResource(R.string.lemon_tree_instructions),
+            text = stringResource(instruction),
             fontSize = 18.sp
         )
 
